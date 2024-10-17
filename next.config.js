110202712fec0656ef42f6afea3bed7781bcc5dc
Tasi <balazstasi2@gmail.com>
2024-10-17 03:59:06 +0200

    fix(): Next config updated to be able to render images from finnhub

--- next.config.js
+++ next.config.js
@@ -1,4 +1,21 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "static2.finnhub.io",
+        protocol: "https",
+      },
+      {
+        hostname: "finnhub.io",
+        protocol: "https",
+      },
+      {
+        hostname: "flagcdn.com",
+        protocol: "https",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
